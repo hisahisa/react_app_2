1da2102a839cb378b0b325c29b39505b12ae7004
hisahisa <jxrku23@hotmail.com>
2025-02-10 20:44:47 +0900

    fix: refactor

--- src/common/useCookieState.js
+++ src/common/useCookieState.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect } from "react";
 import { useCookies } from "react-cookie";
 
 const useCookieState = (key, initialValue) => {
